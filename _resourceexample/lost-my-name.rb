71e1f7a4ca90d05bd346a01fc0657ea508e0354d
Marc Thomas <hello@mrcthms.com>
2017-05-16 13:08:42 +0100

    Add Lost My Name's Design System as a resource

---
+++ _resourceexample/lost-my-name.rb
@@ -0,0 +1,11 @@
+---
+title: Lost My Name
+link: http://design-system.lostmy.name/
+image: lostmyname.jpg
+tags:
+- frontend
+- code
+- patterns
+---
+
+The Design System used throughout the digital design & frontend engineering teams at Lost My Name, complete with design principles and an interactive component library.
